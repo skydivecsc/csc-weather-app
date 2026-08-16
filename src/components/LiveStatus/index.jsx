4e19eff0a22f50025b37414d599033f33b405191
skydivecsc <146664386+skydivecsc@users.noreply.github.com>
2026-08-15 17:30:15 -0700

    Upgrade weather routing: update src/components/LiveStatus/index.jsx

--- src/components/LiveStatus/index.jsx
+++ src/components/LiveStatus/index.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router";
 import { useEffect, useState, useContext } from "react";
 import { WeatherContext } from "../../context/WeatherContext";
 import live from "../../images/live.png";
